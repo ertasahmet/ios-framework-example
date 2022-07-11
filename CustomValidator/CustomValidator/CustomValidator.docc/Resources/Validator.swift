99aa8af7fd0a7702e8d9c30fc704646905d57296
ahmetertas1 <ahmet.ertas@pronet.com.tr>
2022-07-11 17:11:35 +0300

    Add email validate

--- CustomValidator/CustomValidator/CustomValidator.docc/Resources/Validator.swift
+++ CustomValidator/CustomValidator/CustomValidator.docc/Resources/Validator.swift
@@ -9,7 +9,14 @@ import Foundation
 
 public struct Validator {
     
-    public static func isEmailValid(_ email: String) -> Bool {
+    public static func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    public static func isPasswordValid(_ email: String) -> Bool {
         return true
     }
     
